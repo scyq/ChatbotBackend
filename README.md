772dd5eac3cf7b975ff1b586ea52993e426a579d
scyq <chenyuqing0429@foxmail.com>
2022-05-05 23:28:39 +0800

    database published

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ChatbotBackend
+
+``` shell
+    python -m uvicorn main:app --host '0.0.0.0' --port 8000 --reload
+```
---
+++ main.py
@@ -0,0 +1,61 @@
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import JSONResponse
+
+from sql import crud, database, models, schemas
+from sql.database import db_state_default
+
+database.db.connect()
+database.db.create_tables([models.ScaleRecord])
+database.db.close()
+
+app = FastAPI()
+''' 允许跨域访问 '''
+headers = {"Access-Control-Allow-Origin": "*"}
+
+sleep_time = 3
+
+
+async def reset_db_state():
+    database.db._state._state.set(db_state_default.copy())
+    database.db._state.reset()
+
+
+def get_db(db_state=Depends(reset_db_state)):
+    try:
+        database.db.connect()
+        yield
+    finally:
+        if not database.db.is_closed():
+            database.db.close()
+
+
+@app.get("/")
+def read_root():
+    return JSONResponse(content="Hello World", headers=headers)
+
+
+@app.get("/record/{record_id}",
+         response_model=schemas.ScaleRecord,
+         dependencies=[Depends(get_db)])
+def read_record(record_id: int):
+    db_record = crud.get_record(record_id=record_id)
+    if db_record is None:
+        raise HTTPException(status_code=404, detail="Record not found")
+    return db_record
+
+
+@app.get("/records/{scale_id}",
+         response_model=List[schemas.ScaleRecord],
+         dependencies=[Depends(get_db)])
+def read_records_by_scale_id(scale_id: str):
+    records = crud.get_records_by_scaleid(scale_id=scale_id)
+    return records
+
+
+# 这里暂时先不用一个接口通过get和post区分, 因为可能会漏掉
+@app.post("/newrecord/",
+          response_model=schemas.ScaleRecord,
+          dependencies=[Depends(get_db)])
+def create_record(record: schemas.ScaleRecordCreate):
+    return crud.create_scale_record(record=record)
